c3184328f68e9c63b6e26d28a2009d0358dbbcb8
Daniel <druiz07@ucm.es>
2022-11-21 20:06:36 +0100

    Solucionados mas errores de la reestructuracion

--- PracticaFinal/skeleton/Torbellino.h
+++ PracticaFinal/skeleton/Torbellino.h
@@ -7,7 +7,33 @@ public:
 
 	Torbellino() {};
 	Torbellino(const float k1, const float k2, float fuerza, Vector3 cT) :_k1(k1), _k2(k2), kF(fuerza), centroTorbellino(cT) {};
-	virtual void updateForce(Particula* p, float t);
+	virtual void updateForce(Particula* p, float t)
+	{
+
+		if (!p->hasFiniteMass()) return;
+
+
+
+
+
+		Vector3 tVel;
+		tVel.x = kF * (-(p->getPosition().z - centroTorbellino.z) - 0.2 * (p->getPosition().x - centroTorbellino.x));
+		tVel.y = kF * (20 - (p->getPosition().y - centroTorbellino.y));
+		tVel.z = kF * (p->getPosition().x - centroTorbellino.x) - 0.2 * (p->getPosition().z - centroTorbellino.z);
+		Vector3 v = p->getVel() - tVel; //v seria -> getV- velViento , hacer que herede de la clase y un metodo para settear la velocidad , crear un genersdor con vientos 
+
+
+
+		float drag_coef = v.normalize();
+		Vector3 dragF;
+		drag_coef = _k1 * drag_coef + _k2 * drag_coef * drag_coef;
+
+		dragF = -v * drag_coef;  //
+
+
+		p->addForce(dragF);
+
+	};
 	void setTorbCentro(Vector3 c);
 	void  setForce(float nForce) { kF = nForce; }
 	void addForce(float addedForce) { kF = addedForce; }
--- skeleton/Torbellino.cpp
+++ skeleton/Torbellino.cpp
@@ -1,34 +1,6 @@
 
 #include "Torbellino.h"
- void Torbellino::updateForce(Particula* p, float t)
-{
 
-
-	if (fabs(p->inverse_mass) < 1e-10)return;
-
-
-
-
-
-	Vector3 tVel;
-	tVel.x = kF * (-(p->getPosition().z - centroTorbellino.z)- 0.2*(p->getPosition().x - centroTorbellino.x));
-	tVel.y = kF * (20-(p->getPosition().y - centroTorbellino.y));
-	tVel.z = kF*(p->getPosition().x- centroTorbellino.x) - 0.2 * (p->getPosition().z - centroTorbellino.z);
-	Vector3 v = p->getVelocity() - tVel; //v seria -> getV- velViento , hacer que herede de la clase y un metodo para settear la velocidad , crear un genersdor con vientos 
-	
-	
-	
-	float drag_coef = v.normalize();
-	Vector3 dragF;
-	drag_coef = _k1 * drag_coef + _k2 * drag_coef * drag_coef;
-
-	dragF = -v * drag_coef;  //
-
-
-	p->addForce(dragF);
-
-
-};
 void Torbellino::setTorbCentro(Vector3 c)
 {
 	centroTorbellino = c;
