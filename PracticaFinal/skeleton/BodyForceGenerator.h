f1149a617dd5fe19b05c19c1c2f4f19f505e0a0d
Daniel <druiz07@ucm.es>
2023-01-19 22:09:13 +0100

    VACIADO DEL GITHUB

--- PracticaFinal/skeleton/BodyForceGenerator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "SolidBody.h"
-
-class BodyForceGenerator
-{
-public:
-	// Overload to provide functionality
-	virtual void updateForce(SolidBody* solidBody, float t) = 0;
-};
--- PracticaFinal/skeleton/ParticleForceGenerator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Particula.h"
-
-class ParticleForceGenerator
-{
-public:
-	// Overload to provide functionality
-	virtual void updateForce(Particula* particula, float t) = 0;
-};
